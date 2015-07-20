6b67c466047c2af11c632b1791a7df5b59099f16
treystaff <treystaff@gmail.com>
2015-07-20 10:03:08 -0500

    Updated a few things to make plots easier to generate. Also fixed x-axis date readability issue

--- Python/create_greenness_plots.py
+++ Python/create_greenness_plots.py
@@ -6,20 +6,11 @@ import greenness as grn
 from phenocam_toolkit import *
 import pickle
 import pdb
-
-
-# USER INPUT
-phenoDir = '/storage/phenocamdata/'  # Path to phenocam archives.
-#sitename = 'ninemileprairie'  # The sitename of the PhenoCam site.
-sitename = 'kansas'
-roi_path = '/storage/kansas_mask.tif'
-#roi_path = '/storage/ninemile_mask.tif'
-saved_data_path = None
-save_data_path = '/tmp/gcc_data.pickle'
+import matplotlib.pyplot as plt
 
 '''***AUTOMATED SECTION BELOW***'''
 # Calculate data from images, or load?
-if not saved_data_path:
+def save_image_data(phenoDir, sitename, save_data_path, roi_path=None):
     # Open the mask if exists
     if roi_path:
         roi = Image.open(roi_path)
@@ -68,12 +59,44 @@ if not saved_data_path:
         with open(save_data_path, 'w') as savefile:
             pickle.dump(data_dict, savefile)
 
-# Open/load the existing data
-else:
+def load_image_data(saved_data_path):
+    # Open/load the existing data
     with open(saved_data_path) as savefile:
         data_dict = pickle.load(savefile)
-    gcc = data_dict['gcc']
-    dates = data_dict['dates']
+        gcc = data_dict['gcc']
+        dates = data_dict['dates']
+        return gcc, dates
 
-# Now we have the gcc and dates, calculate per90
-per90_gcc = grn.per90(dates, gcc)
+def create_plots(sitename, saved_data_path):
+    # Load the data
+    gcc, dates = load_image_data(saved_data_path)
+    # Now we have the gcc and dates, calculate per90
+    per90_gcc, per90_dates = grn.per90(dates, gcc)
+
+    # Do some plotting
+    fig, ax = plt.subplots(1)
+    ax.plot(dates, gcc, 'b.', label='Raw GCC')
+    ax.plot(per90_dates, per90_gcc, 'go', label='Per90 GCC', markersize=8)
+    plt.title(sitename + ' Raw GCC vs 3 Day Per90 GCC')
+    plt.xlabel('Date')
+    plt.ylabel('GCC')
+    plt.legend(loc='best', numpoints=1)
+    fig.autofmt_xdate()  # Makes the xaxis dates easier to read.
+
+if __name__ == "__main__":
+    # USER INPUT
+    # phenoDir = '/storage/PhenoCam/images/phenocamdata/'  # Path to phenocam archives.
+    # sitename = 'kansas'
+    # roi_path = '/storage/PhenoCam/images/kansas_mask.tif'
+    # roi_path = '/storage/PhenoCam/images/ninemile_mask.tif'
+
+    # Nine Mile
+    sitename = 'ninemileprairie'
+    saved_data_path = '/storage/PhenoCam/data/ninemile_gcc_data.pickle'
+    create_plots(sitename, saved_data_path)
+    # Kansas
+    sitename = 'kansas'
+    saved_data_path = '/storage/PhenoCam/data/kansas_gcc_data.pickle'
+    create_plots(sitename, saved_data_path)
+    # Show the plots!
+    plt.show()
